de957cc8193ac6a6602eeb3956f54628020225ed
Eger Andreas <andreas.eger.reg@googlemail.com>
2011-02-25 16:30:21 -0800

    views now in haml, sass files
    Signed-off-by: Eger Andreas <andreas.eger.reg@googlemail.com>

---
+++ config.rb
@@ -0,0 +1,12 @@
+require 'susy'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "style"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+preferred_syntax = :sass
