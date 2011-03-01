5d7222d104623b88ee467a69e20b3c896a12553e
sch1zo <andreas.eger.reg@googlemail.com>
2011-03-01 16:18:53 +0100

    reconfigured the app to be modular
    Signed-off-by: sch1zo <andreas.eger.reg@googlemail.com>

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,48 +1,51 @@
-helpers do
-  def pluralize(number, text)
-    return text.pluralize if number != 1
-    text
-  end
-  
-  include Rack::Utils
-  alias_method :h, :escape_html
-  
-  def section(key, *args, &block)
-    @sections ||= Hash.new{ |k,v| k[v] = [] }
-    if block_given?
-      @sections[key] << block
-    else
-      @sections[key].inject(''){ |content, block| content << block.call(*args) } if @sections.keys.include?(key)
+require 'sinatra/base'
+module Sinatra
+  module MyHelper
+    def pluralize(number, text)
+      return text.pluralize if number != 1
+      text
     end
-  end
-  
-  def title(page_title, show_title = true)
-    section(:title) { page_title.to_s }
-    @show_title = show_title
-  end
-
-  def show_title?
-    @show_title
-  end
-  
-  def tag_cloud(classes)
-    tags = Schedule.get_tags
-    max_count = tags.sort_by{|t| t[1] }.last[1]
-
-    tags.each do |tag|
-      index = ((tag[1].to_f / max_count) * (classes.size - 1)).round
-      yield tag[0], classes[index]
+    
+    include Rack::Utils
+    alias_method :h, :escape_html
+    
+    def section(key, *args, &block)
+      @sections ||= Hash.new{ |k,v| k[v] = [] }
+      if block_given?
+        @sections[key] << block
+      else
+        @sections[key].inject(''){ |content, block| content << block.call(*args) } if @sections.keys.include?(key)
+      end
     end
-  end
-  
-  def cache_page(seconds=5*60)
-    response['Cache-Control'] = "public, max-age=#{seconds}" unless development?
-  end
-  
-  def button_to(name, path, method='post')
-    haml <<END
+    
+    def title(page_title, show_title = true)
+      section(:title) { page_title.to_s }
+      @show_title = show_title
+    end
+
+    def show_title?
+      @show_title
+    end
+    
+    def tag_cloud(classes)
+      tags = Schedule.get_tags
+      max_count = tags.sort_by{|t| t[1] }.last[1]
+
+      tags.each do |tag|
+        index = ((tag[1].to_f / max_count) * (classes.size - 1)).round
+        yield tag[0], classes[index]
+      end
+    end
+    
+    def cache_page(seconds=5*60)
+      response['Cache-Control'] = "public, max-age=#{seconds}" unless Sinatra::Base.development?
+    end
+    
+    def button_to(name, path, method='post')
+      haml <<END
 %form{:method => '#{method}', :action => '#{path}'}
   %input{:value => '#{name}', :type => 'submit'}
 END
+    end
   end
 end
